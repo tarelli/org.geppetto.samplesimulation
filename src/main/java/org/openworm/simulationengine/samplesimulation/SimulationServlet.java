d2a0fabda967fa00a9c1d3a659c7e416e245afe4
Giovanni Idili <giovanni.idili@gmail.com>
2012-10-05 11:51:44 +0100

    troubleshooting spring autowiring

--- src/main/java/org/openworm/simulationengine/samplesimulation/SimulationServlet.java
+++ src/main/java/org/openworm/simulationengine/samplesimulation/SimulationServlet.java
@@ -22,22 +22,27 @@ import org.apache.catalina.websocket.WebSocketServlet;
 import org.apache.catalina.websocket.WsOutbound;
 import org.openworm.simulationengine.core.model.HHModel;
 import org.openworm.simulationengine.core.simulator.ISimulator;
+import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.stereotype.Service;
 
 import com.google.gson.Gson;
 
-
+@Configurable(autowire=Autowire.BY_TYPE)
 public class SimulationServlet extends WebSocketServlet {
+	
+	@Autowired
+	public SimulationConfig config;
+	
+	@Autowired
+	public ISimulator sampleSimulatorService;
 
 	private static final long serialVersionUID = 1L;
 
 	private static final long UPDATE_CYCLE = 100;
 
-
-
-	private final Timer _simTimer = new Timer(
-			SimulationServlet.class.getSimpleName() + " Timer");
+	private final Timer _simTimer = new Timer(SimulationServlet.class.getSimpleName() + " Timer");
 
 	private final AtomicInteger _connectionIds = new AtomicInteger(0);
 
@@ -46,8 +51,7 @@ public class SimulationServlet extends WebSocketServlet {
 	private final ConcurrentHashMap<Integer, SimDataInbound> _connections = new ConcurrentHashMap<Integer, SimDataInbound>();
 
 	@Override
-	protected StreamInbound createWebSocketInbound(String subProtocol,
-			HttpServletRequest request) {
+	protected StreamInbound createWebSocketInbound(String subProtocol, HttpServletRequest request) {
 		return new SimDataInbound(_connectionIds.incrementAndGet());
 	}
 
@@ -61,18 +65,15 @@ public class SimulationServlet extends WebSocketServlet {
 				try {
 					update();
 				} catch (RuntimeException e) {
-					// log.error("Caught to prevent timer from shutting down",
-					// e);
+					// log.error("Caught to prevent timer from shutting down", e);
 				}
 			}
 		}, UPDATE_CYCLE, UPDATE_CYCLE);
-
 	}
 
 	private void update() {
 		StringBuilder sb = new StringBuilder();
-		for (Iterator<SessionContext> iterator = getSimulations().iterator(); iterator
-				.hasNext();) {
+		for (Iterator<SessionContext> iterator = getSimulations().iterator(); iterator.hasNext();) {
 			SessionContext simulation = iterator.next();
 
 			if (simulation._models != null) {
@@ -82,18 +83,14 @@ public class SimulationServlet extends WebSocketServlet {
 				for (int t = 0; t < simulation._models.get("0").size(); t++) {
 					// plot from 0
 					if ((t * simulation._timeConfiguration.getTimeStepLength()) >= 0) {
-						xDataset.add(t
-								* SampleSimulationController.config.getSamplingPeriod()
-								* simulation._timeConfiguration
-										.getTimeStepLength());
-						yDataset.add(((HHModel) simulation._models.get("0")
-								.get(t)).getV());
+						xDataset.add(t * config.getSamplingPeriod() * simulation._timeConfiguration.getTimeStepLength());
+						yDataset.add(((HHModel) simulation._models.get("0").get(t)).getV());
 					}
 				}
 
 				Gson gson = new Gson();
 				sb.append("[" + gson.toJson(xDataset) + ","
-						+ gson.toJson(yDataset) + "]"); // Put both objects in n
+							  + gson.toJson(yDataset) + "]"); // Put both objects in n
 														// array of 2 elements
 
 			}
@@ -102,9 +99,8 @@ public class SimulationServlet extends WebSocketServlet {
 				sb.append(',');
 			}
 		}
-		// sendUpdate(String.format("{'type': 'update', 'data' : [%s]}",
+		
 		sendUpdate(sb.toString());
-
 	}
 
 	private void sendUpdate(String message) {
@@ -143,7 +139,6 @@ public class SimulationServlet extends WebSocketServlet {
 			_connections.put(Integer.valueOf(id), this);
 			// tentative support for multiple connections but we'll have to see
 			// how the osgi services can be instantiated
-
 		}
 
 		@Override
@@ -154,15 +149,14 @@ public class SimulationServlet extends WebSocketServlet {
 
 		@Override
 		protected void onBinaryMessage(ByteBuffer message) throws IOException {
-			throw new UnsupportedOperationException(
-					"Binary message not supported.");
+			throw new UnsupportedOperationException("Binary message not supported.");
 		}
 
 		@Override
 		protected void onTextMessage(CharBuffer message) throws IOException {
 			String msg = message.toString();
 			if (msg.equals("start")) {
-				new SimulationThread(_sessionContext).start();
+				new SimulationThread(_sessionContext, config, sampleSimulatorService).start();
 			} else if (msg.equals("stop")) {
 				_sessionContext._runSimulation = false;
 				_sessionContext._runningCycle = false;
--- src/main/java/org/openworm/simulationengine/samplesimulation/SimulationThread.java
+++ src/main/java/org/openworm/simulationengine/samplesimulation/SimulationThread.java
@@ -3,8 +3,6 @@ package org.openworm.simulationengine.samplesimulation;
 import java.util.HashMap;
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openworm.simulationengine.core.model.HHModel;
@@ -13,112 +11,112 @@ import org.openworm.simulationengine.core.simulation.ISimulation;
 import org.openworm.simulationengine.core.simulation.ISimulationCallbackListener;
 import org.openworm.simulationengine.core.simulation.TimeConfiguration;
 import org.openworm.simulationengine.core.simulator.ISimulator;
-import org.springframework.beans.factory.annotation.Autowired;
 
 class SimulationThread extends Thread implements ISimulation, ISimulationCallbackListener {
-		
+
 	private static Log logger = LogFactory.getLog(SimulationThread.class);
-	
-	private SessionContext _sessionContext = null;
-		
-		public SimulationThread(SessionContext sessionContext){
-			_sessionContext = sessionContext;
-		}
-		
-		private SessionContext getSessionContext(){
-			return _sessionContext;
-		}
-		
-		/* (non-Javadoc)
-		 * @see org.openworm.simulationengine.core.simulation.ISimulationCallbackListener#resultReady(java.util.List)
-		 */
-		@Override
-		public void resultReady(final List<IModel> models) 
-		{
-			//when the callback is received results are appended
-			appendResults(models);
+
+	private SessionContext sessionContext = null;
+
+	public ISimulator sampleSimulatorService;
+
+	public SimulationConfig config;
+
+	public SimulationThread(SessionContext context, SimulationConfig config, ISimulator sampleSimulatorService) {
+		this.sessionContext = context;
+		this.config = config;
+		this.sampleSimulatorService = sampleSimulatorService;
+	}
+
+	private SessionContext getSessionContext() {
+		return sessionContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.openworm.simulationengine.core.simulation.ISimulationCallbackListener
+	 * #resultReady(java.util.List)
+	 */
+	@Override
+	public void resultReady(final List<IModel> models) {
+		// when the callback is received results are appended
+		appendResults(models);
+	}
+
+	/**
+	 * @param models
+	 */
+	private void appendResults(List<IModel> models) {
+		String receivedId = models.get(0).getId();
+		if (getSessionContext()._models == null) {
+			getSessionContext()._models = new HashMap<String, List<IModel>>();
 		}
 
-		/**
-		 * @param models
-		 */
-		private void appendResults(List<IModel> models) 
-		{			
-			String receivedId = models.get(0).getId();
-			if(getSessionContext()._models==null)
-			{
-				getSessionContext()._models=new HashMap<String,List<IModel>>();
-			}
-
-			if(getSessionContext()._models.containsKey(receivedId))
-			{
-				// check if we have more steps than can be displayed
-				if(getSessionContext()._models.get(receivedId).size() >= (SampleSimulationController.config.getViewport() / SampleSimulationController.config.getDt())/SampleSimulationController.config.getSamplingPeriod())
-				{
-					// if we have more steps that can be displayed - remove the difference
-					for(int i=0;i<models.size();i++)
-					{
-						// always remove the first - when removing everything gets shifted
-						getSessionContext()._models.get(receivedId).remove(0);
-					}
+		if (getSessionContext()._models.containsKey(receivedId)) {
+			// check if we have more steps than can be displayed
+			if (getSessionContext()._models.get(receivedId).size() >= (config.getViewport() / config.getDt()) / config.getSamplingPeriod()) {
+				// if we have more steps that can be displayed - remove the
+				// difference
+				for (int i = 0; i < models.size(); i++) {
+					// always remove the first - when removing everything gets
+					// shifted
+					getSessionContext()._models.get(receivedId).remove(0);
 				}
-				
-				// add all the timesteps for the model
-				getSessionContext()._models.get(receivedId).addAll(models);
 			}
-			else
-			{
-				getSessionContext()._models.put(receivedId,models);
-			}
-			
-			getSessionContext()._processedElements = getSessionContext()._processedElements + 1;
-			
-			// NOTE: this needs to be set only when all the elements have been processed
-			if(getSessionContext()._processedElements == SampleSimulationController.config.getElemCount()){
-				getSessionContext()._runningCycle = false;
+
+			// add all the timesteps for the model
+			getSessionContext()._models.get(receivedId).addAll(models);
+		} else {
+			getSessionContext()._models.put(receivedId, models);
+		}
+
+		getSessionContext()._processedElements = getSessionContext()._processedElements + 1;
+
+		// NOTE: this needs to be set only when all the elements have been
+		// processed
+		if (getSessionContext()._processedElements == config.getElemCount()) {
+			getSessionContext()._runningCycle = false;
+		}
+	}
+
+	public void run() {
+		getSessionContext()._runSimulation = true;
+
+		while (getSessionContext()._runSimulation) {
+			if (!getSessionContext()._runningCycle) {
+				getSessionContext()._runningCycle = true;
+				getSessionContext()._processedElements = 0;
+
+				logger.debug("start simulation");
+				int ELEM_COUNT = config.getElemCount();
+
+				sampleSimulatorService.initialize(this);
+				sampleSimulatorService.startSimulatorCycle();
+
+				getSessionContext()._timeConfiguration = new TimeConfiguration(
+						(float) config.getDt(), config.getSteps(),
+						config.getSamplingPeriod());
+
+				// create the models to be simulated
+				for (int j = 0; j < ELEM_COUNT; j++) {
+					HHModel modelToSimulate;
+					if (getSessionContext()._models == null) {
+						// initial condition
+						modelToSimulate = new HHModel(Integer.toString(j),config.getV(), config.getXn(), config.getXm(),config.getXh(),getSessionContext()._externalCurrent);
+					} else {
+						modelToSimulate = (HHModel) getSessionContext()._models.get(Integer.toString(j)).get(getSessionContext()._models.get(Integer.toString(j)).size() - 1);
+						modelToSimulate.setI(getSessionContext()._externalCurrent);
+					}
+
+					// this is where the simulation hooks up with the solver
+					sampleSimulatorService.simulate(modelToSimulate,getSessionContext()._timeConfiguration);
+				}
+
+				sampleSimulatorService.endSimulatorCycle();
+				logger.debug("end simulation");
 			}
 		}
-		
-        public void run() 
-        {
-        	getSessionContext()._runSimulation=true;
-        	
-    		while(getSessionContext()._runSimulation)
-    		{
-    			if(!getSessionContext()._runningCycle)
-    			{
-    				getSessionContext()._runningCycle = true;
-    				getSessionContext()._processedElements = 0;
-    				
-    				logger.debug("start simulation");
-    				int ELEM_COUNT = SampleSimulationController.config.getElemCount();
-
-    				SampleSimulationController.sampleSimulatorService.initialize(this);
-    				SampleSimulationController.sampleSimulatorService.startSimulatorCycle();
-    				
-    				getSessionContext()._timeConfiguration = new TimeConfiguration((float) SampleSimulationController.config.getDt(), SampleSimulationController.config.getSteps(), SampleSimulationController.config.getSamplingPeriod());
-
-    				// create the models to be simulated
-    				for (int j = 0; j < ELEM_COUNT; j++) {
-    					HHModel modelToSimulate;
-    					if(getSessionContext()._models==null)
-    					{
-    						//initial condition
-    						modelToSimulate=new HHModel(Integer.toString(j), SampleSimulationController.config.getV(), SampleSimulationController.config.getXn(), SampleSimulationController.config.getXm(), SampleSimulationController.config.getXh(), getSessionContext()._externalCurrent);
-    					}
-    					else
-    					{
-    						modelToSimulate=(HHModel)getSessionContext()._models.get(Integer.toString(j)).get(getSessionContext()._models.get(Integer.toString(j)).size()-1);
-    						modelToSimulate.setI(getSessionContext()._externalCurrent);
-    					}
-    					
-    					//this is where the simulation hooks up with the solver
-    					SampleSimulationController.sampleSimulatorService.simulate(modelToSimulate,getSessionContext()._timeConfiguration);			
-    				}
-
-    				SampleSimulationController.sampleSimulatorService.endSimulatorCycle();
-    				logger.debug("end simulation");		
-    			}
-    		}
-        }
-    }
+	}
+}
